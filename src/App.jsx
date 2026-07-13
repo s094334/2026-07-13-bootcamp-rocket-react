e27fcaa622545bea5cb8957ebb48171b462ffda7
Chelsea <s094334@gmail.com>
2026-07-13 17:12:37 +0800

    feat: add three program content

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header'
 import AboutRocket from './components/AboutRocket'
 import SuccessCase from './components/SuccessCase'
+import AboutTraining from './components/AboutTraining'
 import Footer from './components/Footer'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <AboutRocket />
       <SuccessCase />
+      <AboutTraining />
       <Footer />
     </>
   )
