c4507a2ce817d866f80cdfcfe71d882966b3b009
Chelsea <s094334@gmail.com>
2026-07-13 20:32:19 +0800

    refactor: extract AboutPositions card component

--- src/components/AboutTraining.jsx
+++ src/components/AboutTraining.jsx
@@ -1,12 +1,17 @@
-const AboutPositons = ({ title, aboutTitle, positionContent, positionHighlight, requirements, skills }) => {
+import backgroundImage from "../assets/rocket-page/training/training-bg.png"
+import trainingFrontend from "../assets/rocket-page/training/training-img-frontend-lg.svg"
+import trainingBackend from "../assets/rocket-page/training/training-img-backend-lg.svg"
+import trainingUI from "../assets/rocket-page/training/training-img-ui-lg.svg"
+
+const AboutPositons = ({ title, reverse, aboutTitle, positionContent, positionHighlight, requirements, skills, img, imgAlt }) => {
   return (
-    <div className="w-full bg-[url(/src/assets/rocket-page/training/training-bg.png)] bg-neutral-white text-neutral-700 px-12 pb-[34px] pt-[50px] md:pt-16 md:pb-10 md:pr-20 md:pl-12 md:rounded-border-m border-neutral-200 border-2 relative">
+    <div style={{ backgroundImage: `url(${backgroundImage})` }} className="w-full bg-neutral-white text-neutral-700 px-12 pb-[34px] pt-[50px] md:pt-16 md:pb-10 md:pr-20 md:pl-12 md:rounded-border-m border-neutral-200 border-2 relative">
       <div className="inline-block border-2 border-neutral-700 rounded-border-s shadow-[4px_4px_0px_0px_#363636] bg-neutral-white px-5 md:px-10 py-3 absolute -top-8">
         <h3 className="font-bold text-mobile-body1 md:text-desktop-body1 text-neutral-700">
           { title }
         </h3>
       </div>
-      <div className="flex flex-col-reverse items-center gap-7 md:flex-row md:justify-between md:gap-12">
+      <div className={`flex flex-col-reverse items-center gap-7 md:justify-between md:gap-12 ${ reverse ? "md:flex-row-reverse" : "md:flex-row"}`}>
         <div className="flex flex-col gap-8 md:gap-9 max-w-[618px]">
           <div className="flex flex-col gap-5">
             <h3 className="font-bold border-neutral-700 text-mobile-body2 md:text-desktop-body2 border-b-3 self-start">
@@ -40,8 +45,8 @@ const AboutPositons = ({ title, aboutTitle, positionContent, positionHighlight, 
           </div>
         </div>
         <img
-          src="src/assets/rocket-page/training/training-img-frontend-lg.svg"
-          alt="Training image"
+          src={ img }
+          alt={ imgAlt }
           className="max-w-[218px] md:max-w-[282px]"
         />
       </div>
@@ -54,6 +59,7 @@ function AboutTraining() {
     {
       id: 1,
       title: "前端工程師",
+      reverse: false,
       aboutTitle: "關於前端",
       positionContent: "主要專精 JavaScript 前端 (65%)、後端 (35%) 開發，投入語言為 HTML、CSS、JavaScript，",
       positionHighlight: "適合有一些 HTML、CSS、JS 基礎的學員報名。",
@@ -69,31 +75,39 @@ function AboutTraining() {
         "後端開發技能：Node.js、MongoDB、heroku、express、JWT",
         "JS 框架：React(Hooks)、Vue3，會依照未來你想投入的公司進行調整",
         "程式版控：Git、GitHub、Git Flow 工作流程"
-      ]
+      ],
+      img: trainingFrontend,
+      imgAlt : "training front-end image"
     },
     {
       id: 2,
       title: "後端工程師",
+      reverse: true,
       aboutTitle: "關於後端",
       positionContent: "後端工程師組主要專注為後端開發，投入後端語言為 ",
-      positionHighlight: "C#、.Net，適合無程式背景的學員報名。",
+      positionHighlight: "C#、.Net，適合無程式背景的學員報名",
       skills: [
         "後端語言：C#",
         "程式架構：ASP.NET Web Form、ASP.NET MVC、Web API",
         "資料庫與雲端主機：MSSQL、Microsoft Azure"
-      ]
+      ],
+      img: trainingBackend,
+      imgAlt : "training Back-end image"
     },
     {
       id: 3,
       title: "UI 設計師",
+      reverse: false,
       aboutTitle: "關於設計",
-      positionContent: "關於主要以網頁 UI 設計 50%、網頁前端 50% 的雙技能樹養成，",
-      positionHighlight: "適合具備視覺背景，操作過繪圖軟體尤佳，歡迎無工作經驗的學員報名。",
+      positionContent: "主要以網頁 UI 設計 50%、網頁前端 50% 的雙技能樹養成，",
+      positionHighlight: "適合具備視覺背景，操作過繪圖軟體尤佳，歡迎無工作經驗的學員報名",
       skills: [
         "網頁前置規劃：Logic Flow、Wireframe、網格系統、使用者體驗",
         "介面繪製流程：Figma 操作、Mockup、Prototype、標註文件",
         "網頁切版技能：HTML5、CSS、Bootstrap5"
-      ]
+      ],
+      img: trainingUI,
+      imgAlt : "training UI image"
     }
   ]
   return (
@@ -122,90 +136,6 @@ function AboutTraining() {
             })
           }
         </div>
-        {/* <div className="mt-10 mb-[66px] pl-[49px] md:pl-0 md:mt-16 md:mb-32">
-          <h2 className="font-bold text-neutral-700 text-mobile-h3 md:text-desktop-h3">
-            教練團
-          </h2>
-          <div className="flex gap-6 mb-7">
-            <a href="#">
-              <img
-                src="src/assets/rocket-page/coach/coach-weijie.svg"
-                alt="Weijie photo"
-              />
-            </a>
-            <a href="#">
-              <img
-                src="src/assets/rocket-page/coach/coach-yinmin.svg"
-                alt="Yinmin photo"
-              />
-            </a>
-            <a href="#">
-              <img
-                src="src/assets/rocket-page/coach/coach-justin.svg"
-                alt="Justin photo"
-              />
-            </a>
-            <a href="#">
-              <img
-                src="src/assets/rocket-page/coach/coach-casper.svg"
-                alt="Casper photo"
-              />
-            </a>
-          </div>
-          <h3 className="font-bold text-mobile-h4 md:text-desktop-body1 mb-3">
-            前端教練 | 廖洧杰
-          </h3>
-          <ul className="list-disc list-inside font-normal text-desktop-body3 text-neutral-700 [&>li]:pl-4 [&>li]:-indent-4">
-            <li>
-              2016-2022 過往經歷：
-              <a
-                href="https://www.hexschool.com/"
-                target="_blank"
-                className="underline"
-              >
-                六角學院校長
-              </a>
-              、
-              <a
-                href="https://www.facebook.com/profile.php?id=100039975056467#"
-                target="_blank"
-                className="underline"
-              >
-                高雄火箭隊
-              </a>
-              前端教練
-            </li>
-            <li>2013-2019 成功案例：協助無資訊背景轉職工程師人數超過 500 位</li>
-            <li>2013-2019 授課人數：線上+線下授課學員超過 25,000 位</li>
-            <li>
-              2014-2019 線下授課：
-              <a
-                href="https://www.im.nuk.edu.tw/?page_id=95"
-                target="_blank"
-                className="underline"
-              >
-                高雄大學前端領域兼任講師
-              </a>
-            </li>
-            <li>
-              2007-2019 實務經驗：經手超過 100
-              個實際專案，其領域不乏中小企業、政府專案、銀行系統
-            </li>
-          </ul>
-        </div> */}
-        {/* <a
-          href="#"
-          className="max-w-[332px] md:max-w-[1076px] w-full bg-rocket-100 flex justify-center rounded-border-m border-2 border-neutral-700 absolute -bottom-11 py-5 md:py-6 group relative"
-        >
-          <h3 className="font-bold text-mobile-h3 md:text-desktop-h3 text-neutral-700">
-            馬上報名！
-          </h3>
-          <img
-            src="src/assets/rocket-page/ic-go-rocket.svg"
-            alt="Go button logo"
-            className="hidden group-hover:block absolute right-10 bottom-2 md:right-1/3 md:bottom-4"
-          />
-        </a> */}
       </section>
     </>
 
