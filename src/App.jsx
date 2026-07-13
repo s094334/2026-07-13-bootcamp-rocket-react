fb21d2d9d158c2edf5aab6e67d2e304f86bb2051
Chelsea <s094334@gmail.com>
2026-07-13 15:13:44 +0800

    feat: add about rocket section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header'
+import AboutRocket from './components/AboutRocket'
 import Footer from './components/Footer'
 
 function App() {
   return (
     <>
       <Header />
+      <AboutRocket />
       <Footer />
     </>
   )
