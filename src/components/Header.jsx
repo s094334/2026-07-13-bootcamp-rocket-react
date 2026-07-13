20eb2289d1dc3c9e87609e6b2f151d840d92bc8c
Chelsea <s094334@gmail.com>
2026-07-13 10:32:52 +0800

    feat: add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <>
+      <header className="flex justify-center py-2 font-bold [&>p]:px-4 [&>p]:py-3 text-neutral-700 whitespace-nowrap overflow-hidden">
+        <p>Let’s join</p>
+        <p>- 火箭隊培訓營 Rocket -</p>
+        <p>- 共同空間 Co-working space -</p>
+      </header>
+    </>
+  )
+}
+
+export default Header
