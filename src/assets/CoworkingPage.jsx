dcd5c21c38d76af2bfb65d2e3569d9a35eb26975
Chelsea <s094334@gmail.com>
2026-07-14 18:05:47 +0800

    feat: add SpaceIntro and SpaceDescription

--- src/assets/CoworkingPage.jsx
+++ src/assets/CoworkingPage.jsx
@@ -1,10 +1,14 @@
 import Header from "../components/Header"
+import SpaceIntro from "../components/SpaceIntro"
+import SpaceDescription from "../components/SpaceDescription"
 import Footer from "../components/Footer"
 
 function CoworkingPage() {
   return (
     <>
       <Header />
+      <SpaceIntro />
+      <SpaceDescription />
       <Footer />
     </>
   )
