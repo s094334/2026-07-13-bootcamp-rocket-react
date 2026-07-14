b8a6c5c2a6eb87e89e7b5777ec04cec6c5ca2683
Chelsea <s094334@gmail.com>
2026-07-14 18:19:08 +0800

    fix: adjust import image for SpaceIntro

--- src/components/SpaceIntro.jsx
+++ src/components/SpaceIntro.jsx
@@ -1,11 +1,15 @@
+import mapImgLg from "../src/assets/space-page/map-lg.png"
+import mapImgSm from "../src/assets/space-page/map-sm.png" 
+
+
 function SpaceIntro() {
   return (
     <section className="bg-space-200 py-9 px-12 md:py-15 flex flex-col-reverse gap-5 md:flex-row md:gap-24 justify-center items-center text-neutral-700 rounded-border-xl">
       <div className="hidden bg-neutral-white rounded-border-m p-3 max-w-[636px] md:flex items-center justify-center">
-        <img src="src/assets/space-page/map-lg.png" alt="Map image" />
+        <img src={ mapImgLg} alt="Map image" />
       </div>
       <div className="md:hidden bg-neutral-white rounded-border-s p-3 max-w-[332px] flex items-center justify-center">
-        <img src="src/assets/space-page/map-sm.png" alt="Map image" />
+        <img src={ mapImgSm } alt="Map image" />
       </div>
       <div>
         <h3 className="font-bold text-mobile-body3 md:text-desktop-body2">
