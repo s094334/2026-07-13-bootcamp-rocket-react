8492ada12088b83270432faea45dd2048f28b236
Chelsea <s094334@gmail.com>
2026-07-14 16:40:32 +0800

    feat: add CoworkingPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import './App.css'
-import Header from './components/Header'
-import AboutRocket from './components/AboutRocket'
-import SuccessCase from './components/SuccessCase'
-import AboutTraining from './components/AboutTraining'
-import AboutFee from './components/AboutFee'
-import Support from './components/Support'
-import Footer from './components/Footer'
+import CoworkingPage from './assets/CoworkingPage'
 
 function App() {
   return (
     <>
-      <Header />
-      <AboutRocket />
-      <SuccessCase />
-      <AboutTraining />
-      <AboutFee />
-      <Support />
-      <Footer />
+      <CoworkingPage />
     </>
   )
 }
---
+++ src/assets/CoworkingPage.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+
+function CoworkingPage() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default CoworkingPage
