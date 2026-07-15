24dbf969b668440209ce323162a73fc7220d79ee
Chelsea <s094334@gmail.com>
2026-07-15 10:17:42 +0800

    feat: add SpaceFee component

--- src/assets/CoworkingPage.jsx
+++ src/assets/CoworkingPage.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header"
 import SpaceIntro from "../components/SpaceIntro"
 import SpaceDescription from "../components/SpaceDescription"
+import SpaceFee from "../components/SpaceFee"
 import Footer from "../components/Footer"
 
 function CoworkingPage() {
@@ -9,6 +10,7 @@ function CoworkingPage() {
       <Header />
       <SpaceIntro />
       <SpaceDescription />
+      <SpaceFee />
       <Footer />
     </>
   )
